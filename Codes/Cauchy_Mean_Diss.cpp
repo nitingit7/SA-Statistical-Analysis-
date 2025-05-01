f826fab6a49cfd969cd84c33863a05fb4addeee6
Nitin <119484935+nitingit7@users.noreply.github.com>
2025-05-01 20:18:34 +0530

    Update and rename Cauchy_Mean_Diss.c to Cauchy_Mean_Diss.cpp

--- Codes/Cauchy_Mean_Diss.cpp
+++ Codes/Cauchy_Mean_Diss.cpp
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
 
-#define N 1000      // Size of each random sequence
-#define M 10000     // Number of sequences
+#define N 1000  // Size of each random sequence
+#define M 10000 // Number of sequences
+#define M_PI 3.14159265358979323846 // Define PI since some compilers don't include it
 
 // Generate random numbers from a Cauchy-Lorentz distribution
-void generate_cauchy_means(double means[], int size) 
-{
+void generate_cauchy_means(double means[], int size) {
     for (int i = 0; i < size; i++) {
         double sum = 0.0;
         for (int j = 0; j < N; j++) {
-            double u = ((double)rand() / RAND_MAX);
+            double u = (double)rand() / RAND_MAX;
             double z = tan(M_PI * (u - 0.5)); // Cauchy distribution
             sum += z;
         }
@@ -21,11 +21,10 @@ void generate_cauchy_means(double means[], int size)
 }
 
 // Save distribution to a file for plotting
-void plot_distribution(double means[], int size, const char* filename) 
-{
+void plot_distribution(double means[], int size, const char* filename) {
     FILE* file = fopen(filename, "w");
     if (file == NULL) {
-        perror("Failed to open file");
+        printf("Failed to open file\n");
         return;
     }
     for (int i = 0; i < size; i++) {
@@ -34,10 +33,9 @@ void plot_distribution(double means[], int size, const char* filename)
     fclose(file);
 }
 
-int main()
-{
+int main() {
     double means[M];
-    
+
     // Seed random number generator
     srand(time(NULL));
 
@@ -46,5 +44,4 @@ int main()
     plot_distribution(means, M, "cauchy_distribution.txt");
 
     return 0;
-
 }
